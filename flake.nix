d5864e01b875edf0eea0fb44e7e95e02656c5081
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2023-05-22 17:48:00 +0200

    ver 0.0.1

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?tag=nixos-22.11-small"; # that's 21.05
+    utils.url = "github:numtide/flake-utils";
+    utils.inputs.nixpkgs.follows = "nixpkgs";
+    naersk.url = "github:nmattia/naersk";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    #mozillapkgs = {
+    #url = "github:mozilla/nixpkgs-mozilla";
+    #flake = false;
+    #};
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    naersk,
+    rust-overlay,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      #pkgs = nixpkgs.legacyPackages."${system}";
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {inherit system overlays;};
+      rust = pkgs.rust-bin.stable."1.67.1".default.override {
+        targets = ["x86_64-unknown-linux-musl"];
+      };
+
+      # Override the version used in naersk
+      naersk-lib = naersk.lib."${system}".override {
+        cargo = rust;
+        rustc = rust;
+      };
+
+
+      da_packages = with pkgs; [openssl.dev pkg-config];
+    in rec {
+      # `nix build`
+      packages.my-project = naersk-lib.buildPackage {
+        pname = "herdenbot";
+        root = ./.;
+      };
+      defaultPackage = packages.my-project;
+
+      # `nix run`
+      apps.my-project = utils.lib.mkApp {drv = packages.my-project;};
+      defaultApp = apps.my-project;
+
+      # `nix develop`
+      devShell = pkgs.mkShell {
+        buildInputs = da_packages;
+        # supply the specific rust version
+        nativeBuildInputs = [
+          rust
+          pkgs.rust-analyzer
+          pkgs.git
+          pkgs.cargo-udeps
+          pkgs.cargo-audit
+          pkgs.bacon
+        ];
+        shellHook = ''
+        '';
+      };
+    });
+}
+# {
+
---
+++ herdenbot/src/main.rs
@@ -0,0 +1,89 @@
+use teloxide::{prelude::*, utils::command::BotCommands};
+use tokio::process::Command;
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+    let secret_file = "/secrets/herdenbot/bot.json";
+    log::info!("Starting command bot...");
+    let data = std::fs::read_to_string(secret_file)
+        .expect(&format!("Unable to read file {}", secret_file));
+    let data: serde_json::Value = serde_json::from_str(&data).expect("JSON was not well-formatted");
+    let token = data["token"].as_str().expect("Token was not a string");
+    log::info!("Starting throw dice bot...");
+
+    let bot = Bot::new(token);
+
+    BotCommand::repl(bot, answer).await;
+
+    /* teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_dice(msg.chat.id).await?;
+        Ok(())
+    })
+    .await; */
+}
+
+#[derive(BotCommands, Clone)]
+#[command(
+    rename_rule = "lowercase",
+    description = "These commands are supported:"
+)]
+enum BotCommand {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "start the valheim server")]
+    StartValheim,
+    #[command(description = "stop the valheim server")]
+    StopValheim,
+}
+
+async fn answer(bot: Bot, msg: Message, cmd: BotCommand) -> ResponseResult<()> {
+    log::info!("Received {:?}", msg);
+    match cmd {
+        BotCommand::Help => {
+            bot.send_message(msg.chat.id, BotCommand::descriptions().to_string())
+                .await?
+        }
+        BotCommand::StartValheim => {
+            bot.send_message(msg.chat.id, format!("Starting valheim..."))
+                .await?;
+            let output = Command::new("/run/wrappers/bin/sudo")
+                .arg("/run/current-system/sw/bin/systemctl")
+                .arg("start")
+                .arg("valheim-server.service")
+                .output()
+                .await?;
+
+            if output.status.success() {
+                bot.send_message(msg.chat.id, format!("success")).await?
+            } else {
+                let error_message = String::from_utf8_lossy(&output.stderr);
+
+                bot.send_message(msg.chat.id, format!("error {}", error_message))
+                    .await?
+            }
+        }
+        BotCommand::StopValheim => {
+            bot.send_message(msg.chat.id, format!("Stopping valheim"))
+                .await?;
+            let output = Command::new("/run/wrappers/bin/sudo")
+                .arg("/run/current-system/sw/bin/systemctl")
+                .arg("stop")
+                .arg("valheim-server.service")
+                .output()
+                .await?;
+
+            if output.status.success() {
+                bot.send_message(msg.chat.id, format!("success")).await?
+            } else {
+                let error_message = String::from_utf8_lossy(&output.stderr);
+
+                bot.send_message(msg.chat.id, format!("error {}", error_message))
+                    .await?
+            }
+
+        }
+    };
+
+    Ok(())
+}

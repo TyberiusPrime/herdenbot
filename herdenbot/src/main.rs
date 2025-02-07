245a9ca80dc7c9995cc225fb1455213f338238c5
FF <FF>
2025-02-07 20:43:13 +0100

    don't repeat decrypt messages

--- herdenbot/src/main.rs
+++ herdenbot/src/main.rs
@@ -1,45 +1,69 @@
 use once_cell::sync::OnceCell;
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::path::PathBuf;
+use std::sync::atomic::AtomicBool;
 use std::sync::Arc;
 use std::sync::LazyLock;
 use std::sync::Mutex;
-use std::sync::atomic::AtomicBool;
-use warp::Filter;
-use std::path::PathBuf;
 use teloxide::{prelude::*, utils::command::BotCommands};
 use tokio::process::Command;
+use warp::Filter;
 
 static SECRETDATA: OnceCell<serde_json::Value> = OnceCell::new();
 
-
 static DECRYPT_ENABLED: LazyLock<Mutex<bool>> = LazyLock::new(|| Mutex::new(false));
+static LAST_DECRYPT: LazyLock<Mutex<String>> = LazyLock::new(|| Mutex::new("".to_string()));
 
+async fn handle_decrypt_http_request(
+    bot2: Arc<teloxide::Bot>,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    let do_decrypt = *DECRYPT_ENABLED.lock().unwrap();
+    let chat_ids = SECRETDATA.get().expect("Unable to get secret data")["wichtel"]
+        .as_object()
+        .expect("Unable to get wichtel chat ids");
+    let chat_id: i64 = chat_ids
+        .get("Flo")
+        .expect("No flo?")
+        .as_number()
+        .expect("No flo in chat id")
+        .as_i64()
+        .unwrap();
+    let chat_id = teloxide::types::ChatId(chat_id.into());
+    let mut send = false;
+    let msg = format!("Decrypt requested. Result was: {do_decrypt}");
+    {
+        let mut last = LAST_DECRYPT.lock().unwrap();
+        if msg != *last {
+            *last = msg.clone();
+            send = true;
+        }
+    }
+    if send {
+        bot2.send_message(chat_id, msg).await.unwrap();
+    }
 
-async fn handle_decrypt(bot2: Arc<teloxide::Bot>) -> Result<impl warp::Reply, warp::Rejection> { 
-        let do_decrypt = *DECRYPT_ENABLED.lock().unwrap();
-        let chat_ids = SECRETDATA.get().expect("Unable to get secret data")["wichtel"]
-                .as_object()
-                .expect("Unable to get wichtel chat ids");
-        let chat_id: i64 = chat_ids.get("Flo").expect("No flo?").as_number().expect("No flo in chat id").as_i64().unwrap();
-        let chat_id = teloxide::types::ChatId(chat_id.into());
-        bot2.send_message(chat_id, format!("Decrypt requested. Result was: {do_decrypt}"))
-                        .await.unwrap(); 
-
-        let key = SECRETDATA.get().and_then(|x| x["zfs_keys"].as_object()).and_then(|x| x["sami"].as_str()).expect("No key found").to_string();
-        if do_decrypt {
-      Ok(warp::reply::with_status(key, warp::http::StatusCode::OK))
+    let key = SECRETDATA
+        .get()
+        .and_then(|x| x["zfs_keys"].as_object())
+        .and_then(|x| x["sami"].as_str())
+        .expect("No key found")
+        .to_string();
+    if do_decrypt {
+        Ok(warp::reply::with_status(key, warp::http::StatusCode::OK))
     } else {
-        Ok(warp::reply::with_status("Forbidden".to_string(), warp::http::StatusCode::FORBIDDEN))
+        Ok(warp::reply::with_status(
+            "Forbidden".to_string(),
+            warp::http::StatusCode::FORBIDDEN,
+        ))
     }
 }
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
     //get secret file from env
-    let secret_file = std::env::var("SECRET_FILE").unwrap_or_else(|_| {
-        "/secrets/herdenbot/bot.json".to_string()
-    });
+    let secret_file =
+        std::env::var("SECRET_FILE").unwrap_or_else(|_| "/secrets/herdenbot/bot.json".to_string());
     log::info!("Starting command bot...");
     let data = std::fs::read_to_string(&secret_file)
         .expect(&format!("Unable to read file {}", secret_file));
@@ -54,24 +78,26 @@ async fn main() {
     let bot = Arc::new(Bot::new(token));
     let bot2 = bot.clone();
     let bot_filter = warp::any().map(move || bot.clone());
-    let hello = warp::path!("sami").and(warp::get()).and(bot_filter).and_then(handle_decrypt);
+    let hello = warp::path!("sami")
+        .and(warp::get())
+        .and(bot_filter)
+        .and_then(handle_decrypt_http_request);
 
-    let http_server = warp::serve(hello).run(([0,0,0,0],51822));
+    let http_server = warp::serve(hello).run(([0, 0, 0, 0], 51822));
 
     let j1 = tokio::task::spawn(async {
         BotCommand::repl(bot2, answer).await;
     });
-    let j2 = tokio::task::spawn( async {
+    let j2 = tokio::task::spawn(async {
         http_server.await;
     });
     tokio::spawn(async move {
-    tokio::signal::ctrl_c().await.unwrap();
+        tokio::signal::ctrl_c().await.unwrap();
         //exit process
         std::process::exit(0);
     });
     j1.await.unwrap();
     j2.await.unwrap();
-
 
     /* teloxide::repl(bot, |bot: Bot, msg: Message| async move {
         bot.send_dice(msg.chat.id).await?;
@@ -174,8 +200,14 @@ async fn answer(bot: Arc<Bot>, msg: Message, cmd: BotCommand) -> ResponseResult<
                 if !wichtel_file.exists() {
                     //wuerfeln..
                     let mut forbidden: HashSet<(String, String)> = HashSet::new();
-                    for ab in SECRETDATA.get().expect("unable to get secret data")["forbidden"].as_array().expect("forbidden not arrays") {
-                        forbidden.insert((ab[0].as_str().unwrap().to_string(), ab[1].as_str().unwrap().to_string()));
+                    for ab in SECRETDATA.get().expect("unable to get secret data")["forbidden"]
+                        .as_array()
+                        .expect("forbidden not arrays")
+                    {
+                        forbidden.insert((
+                            ab[0].as_str().unwrap().to_string(),
+                            ab[1].as_str().unwrap().to_string(),
+                        ));
                     }
                     let wichtel = draw_wichtel(
                         &(chat_ids.keys().map(|x| x.to_string()).collect()),
@@ -193,11 +225,16 @@ async fn answer(bot: Arc<Bot>, msg: Message, cmd: BotCommand) -> ResponseResult<
                         serde_json::from_str(&std::fs::read_to_string(wichtel_file)?)
                             .expect("Unable to deserialize wichtel");
                     for (wichtel_from, wichtel_to) in wichtel.iter() {
-                        let receiver = format!("{}", chat_ids.get(wichtel_from).expect("No chat id for wichtel_from").as_number().expect("chat_id target was not a number"));
-                        bot.send_message(
-                            receiver,
-                            format!("Du wichtelst fuer {}", wichtel_to),
-                        ).await?;
+                        let receiver = format!(
+                            "{}",
+                            chat_ids
+                                .get(wichtel_from)
+                                .expect("No chat id for wichtel_from")
+                                .as_number()
+                                .expect("chat_id target was not a number")
+                        );
+                        bot.send_message(receiver, format!("Du wichtelst fuer {}", wichtel_to))
+                            .await?;
                     }
                     bot.send_message(msg.chat.id, format!("Wichteln versendet"))
                         .await?
@@ -210,8 +247,8 @@ async fn answer(bot: Arc<Bot>, msg: Message, cmd: BotCommand) -> ResponseResult<
                 .await?
             }
         }
-        BotCommand::Decrypt => { 
-let chat_ids = SECRETDATA.get().expect("Unable to get secret data")["wichtel"]
+        BotCommand::Decrypt => {
+            let chat_ids = SECRETDATA.get().expect("Unable to get secret data")["wichtel"]
                 .as_object()
                 .expect("Unable to get wichtel chat ids");
 
@@ -223,7 +260,8 @@ let chat_ids = SECRETDATA.get().expect("Unable to get secret data")["wichtel"]
                         .expect("No flo?")
                         .as_number()
                         .expect("No flo in chat id")
-                ) {
+                )
+            {
                 *DECRYPT_ENABLED.lock().unwrap() = true;
                 tokio::spawn(async move {
                     tokio::time::sleep(std::time::Duration::from_secs(150)).await;
@@ -231,7 +269,7 @@ let chat_ids = SECRETDATA.get().expect("Unable to get secret data")["wichtel"]
                     *DECRYPT_ENABLED.lock().unwrap() = false;
                 });
                 bot.send_message(msg.chat.id, format!("Decrypt enabled"))
-                        .await?
+                    .await?
             } else {
                 bot.send_message(
                     msg.chat.id,
@@ -241,7 +279,6 @@ let chat_ids = SECRETDATA.get().expect("Unable to get secret data")["wichtel"]
             }
         }
     };
-
 
     Ok(())
 }
